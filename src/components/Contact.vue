1e127655652bc457f4d7bcb9152c0d2cf00ce93b
mikael.lanceong <mikael.lanceong@gmail.com>
2025-11-12 16:51:37 +0800

    Add iframe size change

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,7 +3,7 @@
 		<h2 class="text-center pb-2">Contact</h2>
 		<div class="row">
 			<div class="col-12 col-md-6 d-flex justify-content-md-end mb-3">
-				<iframe class="p-3 w-75 mx-auto mx-md-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.4100410550705!2d121.0411029757416!3d14.63265027628985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b7f5db7f5e3f%3A0xe15d2d6a4453ed00!2sZuitt%20QC!5e0!3m2!1sen!2sph!4v1759134365240!5m2!1sen!2sph" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				<iframe id="google-map" class="p-3 w-75 mx-auto mx-md-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.4100410550705!2d121.0411029757416!3d14.63265027628985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b7f5db7f5e3f%3A0xe15d2d6a4453ed00!2sZuitt%20QC!5e0!3m2!1sen!2sph!4v1759134365240!5m2!1sen!2sph" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 			</div>
 
 			<div class="col-12 col-md-6">
